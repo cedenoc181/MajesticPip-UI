1f04d5ff9404ca93a81a066ec9228829c78ddc8e
Christian <cedenoc181@gmail.com>
2024-08-15 14:44:52 -0400

    added border to the currency carousel and change banner font size and family

--- src/Components/Currencybanner.jsx
+++ src/Components/Currencybanner.jsx
@@ -10,9 +10,12 @@ console.log(currency);
 console.log("success")
   return (
     <div id="currencybanner">
+      <h2 class="bannerTitle"><span class="usd">1 USD$</span> Conversion</h2>
+       <div class="currencybanner">
           {currency.map((currencies, index) => (
             <Currencybannercards key={index} currencies={currencies}/>
           ))}
+          </div>
     </div>
   )
 }
