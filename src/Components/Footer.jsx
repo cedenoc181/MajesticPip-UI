dd392fde99367d7067456595fa79102205cf3590
Christian <cedenoc181@gmail.com>
2024-08-19 17:20:09 -0400

    finished footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import igIcon from "./images/IG-icon.png"
+import discord from "./images/discord-icon.png"
 import "./Footer.css"
 
 function Footer() {
   return (
     <div id="footerCon" className="footerCon">
 
-     
-      <div>&copy; 2024 MajesticPips.com All rights reserved.</div>
+     <div className="footer row">
+      <h6 className="socialHeader">Connect with us</h6>
+
+       <a className="ig-link column" href="https://www.instagram.com/majesticprodigy?igsh=MTJnZmdmYnVxOWpzdQ==">
+      <img className="instagram column" src={igIcon} alt="IGhandle" />
+      </a>
+
+      <a className="chat-link-footer column" href="#about">
+      <img className="discord-icon" src={discord} alt="discord-icon" />
+    </a>
+     </div>
+      <div className="copyright">&copy; 2024<span className="purple"> MajesticPips</span> All rights reserved.</div>
     </div>
   )
 }
