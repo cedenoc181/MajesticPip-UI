a98b8b92b33d81819a2a551f066f069dd44e971b
Christian <cedenoc181@gmail.com>
2024-08-23 16:56:44 -0400

    all components are optimized for all screenn devies

--- src/Components/Mission.jsx
+++ src/Components/Mission.jsx
@@ -1,41 +1,47 @@
-import React, {useEffect} from 'react'
-import "./Mission.css"
-import AOS from 'aos'
+import React, { useEffect } from "react";
+import "./Mission.css";
+import AOS from "aos";
 
 function Mission() {
-
   useEffect(() => {
-    AOS.init({duration: 1500}); // Reinitialize AOS to account for new content
+    AOS.init({ duration: 1500 }); // Reinitialize AOS to account for new content
   }, []);
-  
 
   return (
     <div id="mission" className="mission">
+      <div className="statement-container">
+        <h2 className="missionTitle" data-aos="zoom-in">
+          Our <span className="purple">Mission</span>
+        </h2>
+        <p className="statement">
+          At MajesticPips, our mission is to empower traders by building a
+          vibrant and supportive currency trading community. We believe in the
+          power of collective knowledge and collaboration. By joining our
+          community, you'll connect with like-minded individuals in our
+          interactive Discord chat, where you can share insights, discuss
+          trading strategies, and refine your approach to the markets.
+        </p>
+        <h3 className="missionTitle2">
+          <span className="purple"> Why</span> us?
+        </h3>
+        <p className="statement2">
+          We are committed to helping you navigate the complexities of currency
+          trading with confidence. Our members gain access to real-time trading
+          signals and alerts, ensuring you're always informed and ready to act
+          on the latest opportunities.
+        </p>
 
-    <div className="statement-container">
-    <h2 className="missionTitle" data-aos="zoom-in">Our <span className="purple">Mission</span></h2>
-    <p className="statement">
-       At MajesticPips, our mission is to empower traders by building a vibrant and supportive currency trading community. 
-       We believe in the power of collective knowledge and collaboration. By joining our community, 
-       you'll connect with like-minded individuals in our interactive Discord chat, where you can share insights, discuss trading strategies,
-       and refine your approach to the markets.
-   </p>
-   <h3 className="missionTitle2"><span className="purple"> Why</span> us?</h3>
-   <p className="statement2">
-   We are committed to helping you navigate the complexities of currency trading with confidence. 
-   Our members gain access to real-time trading signals 
-   and alerts, ensuring you're always informed and ready to act on the latest opportunities.
-   </p>
-
-    <h4 className="missionTitle3"> Our <span className="purple">Ultimate</span> Goal!</h4>
-    <div className="statement3"> 
-    Together, we'll grow as traders, 
-    learning from each other's successes and challenges,
-     and ultimately achieving our financial goals.
+        <h4 className="missionTitle3">
+          {" "}
+          Our <span className="purple">Ultimate</span> Goal!
+        </h4>
+        <div className="statement3">
+          Together, we'll grow as traders, learning from each other's successes
+          and challenges, and ultimately achieving our financial goals.
+        </div>
+      </div>
     </div>
-   </div>
-    </div>
-  )
+  );
 }
 
-export default Mission
+export default Mission;
